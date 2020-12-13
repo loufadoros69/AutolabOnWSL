1a52c73f168939e24f603b2b000560bd1938c341
Nikos Stylianou <68811067+loufadoros69@users.noreply.github.com>
2020-12-14 01:45:55 +0200

    Update script to be semi automatic...

--- autolab.sh
+++ autolab.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/bash
 # kante copy paste to arxeio se enan text editor kai apo8hkeyste to ws autolab.sh
 # molis to katevasete, kante chmod +x autolab.sh gia na mporesei na ektelestei
-# oi fakeloi kai ta directories prepei na alaxtoun apo ton teliko xrhsth
 
+export cwd=$cwd$(pwd)
+cd source_code
+cp dlist.c dlist.h sort.c sort.h sort1.c tree.c tree.h tree1.c tree2.c ../hw2submit/
+cd "$cwd"
 cp -r hw2submit ~/
 cd ~/hw2submit
 chmod a-x *
 cd ../
 tar -czvf hw2submit.tar.gz hw2submit/
-cp hw2submit.tar.gz /mnt/c/Users/Nikos\ Stylianou/Documents/UTh/Domes/HW2/
+cp hw2submit.tar.gz "$cwd"
 rm -rf hw2submit*
-cd /mnt/c/Users/Nikos\ Stylianou/Documents/UTh/Domes/HW2/
+cd "$cwd"
